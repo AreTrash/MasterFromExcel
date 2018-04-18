3d7fa9e402c92020c71f46e3adc07de0402b8d1d
AreTrash <tinpuny44@gmail.com>
2018-04-18 09:41:18 +0900

    fix: path

--- Assets/MasterFromExcel/MfeConst.cs
+++ Assets/MasterFromExcel/MfeConst.cs
@@ -4,7 +4,7 @@
     {
         public const string AssemblyName = "Assembly-CSharp";
         public const string MasterNamespace = @"Master";
-        public const string MasterExcelDirectory = @"../MasterData/";
+        public const string MasterExcelDirectory = @"MasterData/";
         public const string ScriptableObjectOutputDirectory = @"Assets/Resources/MasterSample/"; //Resources以下に設定してください
         public const string ScriptableObjectScriptOutputDirectory = @"Assets/MasterSample/Data/";
         public const string MasterInstallerOutputPath = @"Assets/MasterSample/MasterInstaller.cs";
